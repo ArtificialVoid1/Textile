f24a2e4d35171918ee68ed1cd7a3d383330dad1b
ArtificialVoid <160069526+ArtificialVoid1@users.noreply.github.com>
2024-11-26 08:35:57 -0500

    e

--- src/Textile/PrintTools.py
+++ src/Textile/PrintTools.py
@@ -2,4 +2,15 @@ from __future__ import annotations
 
 #--------------------------------------------------
 
+from PIL import Image
+
 from console import RICH_CONSOLE, Color3
+
+def printIMG(ImagePath : str) -> None:
+    IMG = Image.open(ImagePath)
+
+    for y in range(IMG.size[1]):
+        for x in range(IMG.size[0]):
+            r, g, b = IMG.getpixel((x, y))
+            NewClr = Color3.from_rgb((r, g, b))
+            RICH_CONSOLE.print('██', Style=NewClr, end='')
