90d095698db407de9f62a4ae1d3792a6840cce4d
ArtificialVoid <160069526+ArtificialVoid1@users.noreply.github.com>
2025-02-28 09:43:26 -0500

    Update PrintTools.py
    added printGradient

--- src/Textile/PrintTools.py
+++ src/Textile/PrintTools.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from PIL import Image
 
-from console import RICH_CONSOLE, Color3
+from console import RICH_CONSOLE, Color3, GradientColor3, GradientKeyframe
 
 def printIMG(ImagePath : str) -> None:
     IMG = Image.open(ImagePath)
@@ -14,3 +14,6 @@ def printIMG(ImagePath : str) -> None:
             r, g, b = IMG.getpixel((x, y))
             NewClr = Color3.from_rgb((r, g, b))
             RICH_CONSOLE.print('██', Style=NewClr, end='')
+
+def printGradient(text : str, Gradient : GradientColor3):
+    
